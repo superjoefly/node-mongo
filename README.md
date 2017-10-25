bcbc9c2b946e70085f24efb8f1858a3c50510582
superjoefly <superjoefly@gmail.com>
2017-10-24 19:22:49 -0600

    Added notes and snippets for Find

---
+++ README.md
@@ -0,0 +1,7 @@
+# Node.js Examples
+
+A Collection of Node.js - MongoDB Example Code
+
+## View Live
+
+To view this project live, go to my [Node.js Examples Page](http://joefly.site/pages/projects/node-mongo/)
--- scripts/app.js
+++ scripts/app.js
@@ -77,22 +77,22 @@
 
 // The _id Field
 
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/mydb';
-
-MongoClient.connect(url, function(err, db) {
-  if(err) throw err;
-  var myObj = [
-    {_id: 1, name: 'Race Car'},
-    {_id: 2, name: 'Barby Doll'},
-    {_id: 3, name: 'He-Man'}
-  ];
-  db.collection('products').insertMany(myObj, function(err, res) {
-    if(err) throw err;
-    console.log(res);
-    db.close();
-  });
-});
+// var MongoClient = require('mongodb').MongoClient;
+// var url = 'mongodb://localhost:27017/mydb';
+//
+// MongoClient.connect(url, function(err, db) {
+//   if(err) throw err;
+//   var myObj = [
+//     {_id: 1, name: 'Race Car'},
+//     {_id: 2, name: 'Barby Doll'},
+//     {_id: 3, name: 'He-Man'}
+//   ];
+//   db.collection('products').insertMany(myObj, function(err, res) {
+//     if(err) throw err;
+//     console.log(res);
+//     db.close();
+//   });
+// });
 
 // Result:
 // { result: { ok: 1, n: 3 },
@@ -102,3 +102,123 @@ MongoClient.connect(url, function(err, db) {
 //      { _id: 3, name: 'He-Man' } ],
 //   insertedCount: 3,
 //   insertedIds: [ 1, 2, 3 ] }
+
+
+
+//////////////////
+
+
+// The findOne() Method
+
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/mydb";
+//
+// MongoClient.connect(url, function(err, db) {
+//   if(err) throw err;
+//   db.collection('customers').findOne({}, function(err, result) {
+//     if(err) throw err;
+//     console.log(result.name);
+//     db.close();
+//   });
+// });
+//
+// // Result: My Company
+
+
+// The find() Method
+
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/mydb";
+//
+// MongoClient.connect(url, function(err, db) {
+//   if(err) throw err;
+//   db.collection('customers').find({}).toArray(function(err, result) {
+//     if(err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
+// });
+
+// Result:
+    // [ { _id: 59ee477f406931606fb2a004,
+    // name: 'My Company',
+    // address: 'Boulder CO' },
+    // { _id: 59ee910440030506aa61d274,
+    // name: 'Jason',
+    // address: '2322 Up St' },
+    // { _id: 59ee910440030506aa61d275,
+    // name: 'Brian',
+    // address: '1289 Down St' },
+    // { _id: 59ee910440030506aa61d276,
+    // name: 'Rebecca',
+    // address: '226 Left St' },
+    // { _id: 59ee910440030506aa61d277,
+    //
+    // name: 'T-Bone',
+    // address: '333 Right St' },
+    // { _id: 59ee910440030506aa61d278,
+    // name: 'Sasquach',
+    // address: '456 Middle St' } ]
+
+
+// Find Some
+
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/mydb";
+//
+// MongoClient.connect(url, function(err, db) {
+//   if(err) throw err;
+//   db.collection('customers').find({}, {
+//     _id: false, name: true, address: true
+//   }).toArray(function(err, result) {
+//     if(err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
+// });
+
+// Result:
+    // [ { name: 'My Company', address: 'Boulder CO' },
+    // { name: 'Jason', address: '2322 Up St' },
+    // { name: 'Brian', address: '1289 Down St' },
+    // { name: 'Rebecca', address: '226 Left St' },
+    // { name: 'T-Bone', address: '333 Right St' },
+    // { name: 'Sasquach', address: '456 Middle St' } ]
+
+
+
+// Accessng Properties of an Object
+
+// Access Address Property of Third Object
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/mydb";
+//
+// MongoClient.connect(url, function(err, db) {
+//   if(err) throw err;
+//   db.collection('customers').find({}, {
+//     _id: false, name: true, address: true
+//   }).toArray(function(err, result) {
+//     if(err) throw err;
+//     console.log(result[3].address);
+//     db.close();
+//   });
+// });
+
+// Result: 226 Left St
+
+// Access Name Property of Second Object
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/mydb";
+//
+// MongoClient.connect(url, function(err, db) {
+//   if(err) throw err;
+//   db.collection('customers').find({}, {
+//     _id: false, name: true, address: true
+//   }).toArray(function(err, result) {
+//     if(err) throw err;
+//     console.log(result[2].name);
+//     db.close();
+//   });
+// });
+
+// Result: Brian
